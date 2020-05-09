1cf866b4fb241a85ecff73ca19547e515d155913
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2020-05-09 11:12:03 -0300

    CreatedProject Communication Editor Control

---
+++ ScaduinoNET/ScaduinoWindows/Editors/ProjectCommunicationEditor.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace ScaduinoNET.ScaduinoWindows.Editors
+{
+    partial class ProjectCommunicationEditor
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.TextBoxHeader = new MetroFramework.Controls.MetroTextBox();
+            this.TextBoxAddress = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.TextBoxDescription = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroGrid1 = new MetroFramework.Controls.MetroGrid();
+            this.TagName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TagAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.metroGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.White;
+            this.splitContainer1.Panel1.Controls.Add(this.TextBoxHeader);
+            this.splitContainer1.Panel1.Controls.Add(this.TextBoxAddress);
+            this.splitContainer1.Panel1.Controls.Add(this.metroLabel4);
+            this.splitContainer1.Panel1.Controls.Add(this.TextBoxDescription);
+            this.splitContainer1.Panel1.Controls.Add(this.metroLabel3);
+            this.splitContainer1.Panel1.Controls.Add(this.metroLabel2);
+            this.splitContainer1.Panel1.Controls.Add(this.metroLabel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.metroGrid1);
+            this.splitContainer1.Size = new System.Drawing.Size(497, 464);
+            this.splitContainer1.SplitterDistance = 160;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // TextBoxHeader
+            // 
+            // 
+            // 
+            // 
+            this.TextBoxHeader.CustomButton.Image = null;
+            this.TextBoxHeader.CustomButton.Location = new System.Drawing.Point(142, 1);
+            this.TextBoxHeader.CustomButton.Name = "";
+            this.TextBoxHeader.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.TextBoxHeader.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.TextBoxHeader.CustomButton.TabIndex = 1;
+            this.TextBoxHeader.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.TextBoxHeader.CustomButton.UseSelectable = true;
+            this.TextBoxHeader.CustomButton.Visible = false;
+            this.TextBoxHeader.Lines = new string[0];
+            this.TextBoxHeader.Location = new System.Drawing.Point(308, 114);
+            this.TextBoxHeader.MaxLength = 32767;
+            this.TextBoxHeader.Name = "TextBoxHeader";
+            this.TextBoxHeader.PasswordChar = '\0';
+            this.TextBoxHeader.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.TextBoxHeader.SelectedText = "";
+            this.TextBoxHeader.SelectionLength = 0;
+            this.TextBoxHeader.SelectionStart = 0;
+            this.TextBoxHeader.ShortcutsEnabled = true;
+            this.TextBoxHeader.Size = new System.Drawing.Size(164, 23);
+            this.TextBoxHeader.TabIndex = 1;
+            this.TextBoxHeader.UseSelectable = true;
+            this.TextBoxHeader.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.TextBoxHeader.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // TextBoxAddress
+            // 
+            // 
+            // 
+            // 
+            this.TextBoxAddress.CustomButton.Image = null;
+            this.TextBoxAddress.CustomButton.Location = new System.Drawing.Point(113, 1);
+            this.TextBoxAddress.CustomButton.Name = "";
+            this.TextBoxAddress.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.TextBoxAddress.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.TextBoxAddress.CustomButton.TabIndex = 1;
+            this.TextBoxAddress.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.TextBoxAddress.CustomButton.UseSelectable = true;
+            this.TextBoxAddress.CustomButton.Visible = false;
+            this.TextBoxAddress.Lines = new string[0];
+            this.TextBoxAddress.Location = new System.Drawing.Point(105, 114);
+            this.TextBoxAddress.MaxLength = 32767;
+            this.TextBoxAddress.Name = "TextBoxAddress";
+            this.TextBoxAddress.PasswordChar = '\0';
+            this.TextBoxAddress.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.TextBoxAddress.SelectedText = "";
+            this.TextBoxAddress.SelectionLength = 0;
+            this.TextBoxAddress.SelectionStart = 0;
+            this.TextBoxAddress.ShortcutsEnabled = true;
+            this.TextBoxAddress.Size = new System.Drawing.Size(135, 23);
+            this.TextBoxAddress.TabIndex = 1;
+            this.TextBoxAddress.UseSelectable = true;
+            this.TextBoxAddress.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.TextBoxAddress.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(246, 116);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(56, 19);
+            this.metroLabel4.TabIndex = 0;
+            this.metroLabel4.Text = "Header:";
+            // 
+            // TextBoxDescription
+            // 
+            // 
+            // 
+            // 
+            this.TextBoxDescription.CustomButton.Image = null;
+            this.TextBoxDescription.CustomButton.Location = new System.Drawing.Point(345, 1);
+            this.TextBoxDescription.CustomButton.Name = "";
+            this.TextBoxDescription.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.TextBoxDescription.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.TextBoxDescription.CustomButton.TabIndex = 1;
+            this.TextBoxDescription.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.TextBoxDescription.CustomButton.UseSelectable = true;
+            this.TextBoxDescription.CustomButton.Visible = false;
+            this.TextBoxDescription.Lines = new string[0];
+            this.TextBoxDescription.Location = new System.Drawing.Point(105, 78);
+            this.TextBoxDescription.MaxLength = 32767;
+            this.TextBoxDescription.Name = "TextBoxDescription";
+            this.TextBoxDescription.PasswordChar = '\0';
+            this.TextBoxDescription.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.TextBoxDescription.SelectedText = "";
+            this.TextBoxDescription.SelectionLength = 0;
+            this.TextBoxDescription.SelectionStart = 0;
+            this.TextBoxDescription.ShortcutsEnabled = true;
+            this.TextBoxDescription.Size = new System.Drawing.Size(367, 23);
+            this.TextBoxDescription.TabIndex = 1;
+            this.TextBoxDescription.UseSelectable = true;
+            this.TextBoxDescription.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.TextBoxDescription.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(36, 114);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(59, 19);
+            this.metroLabel3.TabIndex = 0;
+            this.metroLabel3.Text = "Address:";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel2.Location = new System.Drawing.Point(27, 27);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(181, 25);
+            this.metroLabel2.TabIndex = 0;
+            this.metroLabel2.Text = "Communication Setup";
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(18, 78);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(77, 19);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "Description:";
+            // 
+            // metroGrid1
+            // 
+            this.metroGrid1.AllowUserToResizeRows = false;
+            this.metroGrid1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.metroGrid1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.metroGrid1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.metroGrid1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.metroGrid1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.metroGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.metroGrid1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TagName,
+            this.TagAddress});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.metroGrid1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.metroGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.metroGrid1.EnableHeadersVisualStyles = false;
+            this.metroGrid1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.metroGrid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.metroGrid1.Location = new System.Drawing.Point(0, 0);
+            this.metroGrid1.Name = "metroGrid1";
+            this.metroGrid1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.metroGrid1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.metroGrid1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.metroGrid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.metroGrid1.Size = new System.Drawing.Size(497, 300);
+            this.metroGrid1.TabIndex = 0;
+            // 
+            // TagName
+            // 
+            this.TagName.HeaderText = "Tag Name";
+            this.TagName.Name = "TagName";
+            this.TagName.Width = 350;
+            // 
+            // TagAddress
+            // 
+            this.TagAddress.HeaderText = "Address";
+            this.TagAddress.Name = "TagAddress";
+            // 
+            // ProjectCommunicationEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Extensions = new string[0];
+            this.Name = "ProjectCommunicationEditor";
+            this.Size = new System.Drawing.Size(497, 464);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.metroGrid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private MetroFramework.Controls.MetroTextBox TextBoxHeader;
+        private MetroFramework.Controls.MetroTextBox TextBoxAddress;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroTextBox TextBoxDescription;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroGrid metroGrid1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TagName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TagAddress;
+    }
+}
---
+++ ScaduinoNET/ScaduinoWindows/Editors/ProjectCommunicationEditor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CommonControls;
+
+namespace ScaduinoNET.ScaduinoWindows.Editors
+{
+    public partial class ProjectCommunicationEditor : FileEditor
+    {
+        public ProjectCommunicationEditor()
+        {
+            InitializeComponent();
+            Extensions = new string[] {"cnt"};
+        }
+
+        public override void LoadFile()
+        {
+            base.LoadFile();
+        }
+
+        public override void SaveFile()
+        {
+            base.SaveFile();
+        }
+
+        public override bool FileChanged()
+        {
+            return (true);
+        }
+    }
+}
