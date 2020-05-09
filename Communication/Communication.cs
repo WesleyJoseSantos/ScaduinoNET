c2f891de12a74e5d6286bf22f2b9f4ea24f9c07e
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2020-05-09 10:25:32 -0300

    Added communication generic objects

---
+++ Communication/Communication.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using ScreenEditor.DataBase;
+
+namespace Communication
+{
+    public partial class Communication : Component
+    {
+        private List<ControlTag> tags;
+        UInt32 deviceId;
+
+        public List<ControlTag> Tags { get => tags; set => tags = value; }
+        public uint DeviceId { get => deviceId; set => deviceId = value; }
+
+        public Communication()
+        {
+            InitializeComponent();
+        }
+
+        public Communication(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        public virtual void connect()
+        {
+
+        }
+    }
+}
---
+++ Communication/DataBase/CommunicationSheet.cs
@@ -0,0 +1,17 @@
+﻿using ScreenEditor.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Communication.DataBase
+{
+    class CommunicationSheet
+    {
+        string Description { set; get; }
+        string Address { get; set; }
+        string Header { get; set; }
+        List<ControlTag> Tags { get; set; }
+    }
+}
